bbd57119b5812d5e050eb5e9cf94a82201fabad5
MrXiaoM <coolxiaom95@gmail.com>
2025-09-13 15:50:51 +0800

    prevent no any CPlugin implement exists

--- XConomy-Core/src/main/java/me/yic/xconomy/adapter/comp/CPlugin.java
+++ XConomy-Core/src/main/java/me/yic/xconomy/adapter/comp/CPlugin.java
@@ -7,23 +7,27 @@ import java.io.ByteArrayOutputStream;
 
 @SuppressWarnings("unused")
 public class CPlugin implements iPlugin {
+    public CPlugin() {
+        throw new UnsupportedOperationException("stub");
+    }
+
     @Override
     public boolean getOnlinePlayersisEmpty() {
-        return false;
+        throw new UnsupportedOperationException("stub");
     }
 
     @Override
     public void broadcastMessage(String message) {
-
+        throw new UnsupportedOperationException("stub");
     }
 
     @Override
     public void runTaskAsynchronously(Runnable runnable) {
-
+        throw new UnsupportedOperationException("stub");
     }
 
     @Override
     public void sendPluginMessage(String channel, ByteArrayOutputStream stream) {
-
+        throw new UnsupportedOperationException("stub");
     }
 }
